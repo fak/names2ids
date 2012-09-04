81616321820609f3c308028f34f3d87c050bc6dc
Felix Kruger <fkrueger@ebi.ac.uk>
2012-09-04 17:12:36 +0100

    amended typos in name2id

--- names2ids.py
+++ names2ids.py
@@ -11,25 +11,26 @@ momo.sander@googlemail.com
 def name2id(name, outformat):
 
   import cirpy
-  import chemspipy 
+  import chemspipy
+  import queryDevice  
   
   idstring = None
   source = None
 
   if idstring is None:
-    idstring = cirpy.resolve(name, outformat)
     source = 'NCI'
+    idstring = cirpy.resolve(name, outformat)
   if idstring is None:
+    source = 'ChemSpi'
     chemspid = chemspipy.find_one(name)
     try:
       smiles = chemspid.smiles
       idstring = cirpy.resolve(smiles, outformat)
     except AttributeError:
-      pass
-    source = 'ChemSpi'
+      idstring = None
   if idstring is None:
-    idstring = cirpy.resolve(name, outformat,['name_pattern'])
     source = 'NCI-pattern-match'
+    idstring = cirpy.resolve(name, outformat,['name_pattern'])
   if idstring is None:
     source = None
     idstring = str(idstring)
@@ -37,8 +38,27 @@ def name2id(name, outformat):
     idstring = (idstring.rstrip(),source)
   except AttributeError:
     idstring  = (idstring[0].rstrip(),source)
-    print 'There were multiple results for: ', name, ' using: ', idString[0], '\n', idstring 
-
-            
+    print 'There were multiple results for: ', name, ' using: ', idstring[0], '\n', idstring 
 
   return idstring
+
+""" Convert SMILES to stdinchikey using the NCI resolver."""
+def smiles2stdinchikey(smiles):
+
+  import cirpy
+  import queryDevice
+
+  idstring = None
+  source = None
+
+  if idstring is None:
+    idstring = cirpy.resolve(smiles, 'stdinchikey')
+    source = 'NCI'
+  try:
+    idstring = (idstring.rstrip(),source)
+  except AttributeError:
+    idstring  = (idstring[0].rstrip(),source)
+    print 'There were multiple results for: ', name, ' using: ', idString[0], '\n', idstring
+
+  return idstring
+
